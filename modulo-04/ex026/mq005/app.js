9b495d63fedd7f43fb36c6c7cf62d970b4d35fa5
Gabriel GB <105217214+GabrielGBcode@users.noreply.github.com>
2023-06-21 15:13:44 -0300

    Atualizando exercício de Media Query

--- modulo-04/ex026/mq005/app.js
+++ modulo-04/ex026/mq005/app.js
@@ -5,3 +5,11 @@ function clickMenu() {
     itens.style.display = "block";
   }
 }
+
+function mudouTamanho() {
+  if (window.innerWidth >= 768) {
+    itens.style.display = "block";
+  } else {
+    itens.style.display = "none";
+  }
+}
